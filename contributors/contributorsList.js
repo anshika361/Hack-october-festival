8ff897c7e6ff0c2edd60c04f8d93c0b8d7888562
Anshika Agarwal <76562031+anshika361@users.noreply.github.com>
2025-10-04 15:47:47 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -3,6 +3,8 @@ contributors = [
     id: "2",
     fullname: "Anshika Agarwal",
     username: "https://github.com/anshika361",
+  },
+  {
     id: "3",
     fullname: "Nikhil Meena",
     username: "https://github.com/Nicknack03",
