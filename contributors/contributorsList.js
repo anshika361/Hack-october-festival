7aa20a58ab27db2a4ba74072542186716ae32c4f
Parthasarathy G <83574852+parthasarathy27@users.noreply.github.com>
2025-10-04 15:05:43 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1,5 +1,10 @@
 contributors = [
   {
+    id: "your id number",
+    fullname: "Your Name",
+    username: "Your Github Link",
+  },
+  {
     id: 1,
     fullname: "Parthasarathy",
     username: "https://github.com/parthasarathy27",
